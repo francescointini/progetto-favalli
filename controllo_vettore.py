269e358970cc4ecd51c834ba2d4088f3dd3352ab
Simone <55874493+Simone1696@users.noreply.github.com>
2019-10-01 18:01:33 +0200

    Add files via upload

---
+++ controllo_vettore.py
@@ -0,0 +1,7 @@
+#controllo vettore
+
+def check_array(numero,lista):
+    if not(len(lista)==numero):
+        print("Errore, numero elementi array errato")
+        exit("errore elementi array")
+        
