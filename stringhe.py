e680a75c7aeebf4572cc4eb38dd55425de12b715
MirkoGregorelli <55874770+MirkoGregorelli@users.noreply.github.com>
2019-10-18 17:12:34 +0200

    Add files via upload
    funzione per le combinazioni degli ingressi

---
+++ stringhe.py
@@ -0,0 +1,9 @@
+import os
+
+def stringa(x, y, z):
+    res = []
+    res.append("\t\t" + x + " <= '1'\n\t\t" + y + " <= '0'\n\t\twait for " + z +" ns\n")
+    res.append("\t\t" + x + " <= '0'\n\t\t" + y + " <= '1'\n\t\twait for " + z +" ns\n")
+    res.append("\t\t" + x + " <= '1'\n\t\t" + y + " <= '1'\n\t\twait for " + z +" ns\n")
+    res.append("\t\t" + x + " <= '0'\n\t\t" + y + " <= '0'\n\t\twait for " + z +" ns\n")
+    return res
